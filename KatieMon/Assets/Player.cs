5e8b94408d2e92152b4899489e6cb30cb88eddfc
Alex <mavirk2@gmail.com>
2020-09-11 16:09:54 -0400

    Initial Commit

---
+++ KatieMon/Assets/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D myRigidbody;
+    [SerializeField] float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+    }
+
+    public void HandleMovement()
+    {
+        float x = 0;
+        float y = 0;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            y += speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            x -= speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            y -= speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            x += speed;
+        }
+
+        myRigidbody.velocity = new Vector2(x,y);
+
+    }
+}
---
+++ KatieMon/Assets/WorldGen.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGen : MonoBehaviour
+{
+    [SerializeField] int worldSize;
+    [SerializeField] GameObject selectedTile;
+    [SerializeField] bool gen;
+    [SerializeField] GameObject hoverTile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gen)
+        {
+            gen = false;
+            GenerateWorld();
+        }
+
+        //DetectHoverTile();
+        //ChangeTile();
+    }
+
+    private void DetectHoverTile()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+        if (hit.collider != null)
+        {
+            hoverTile = hit.collider.gameObject;
+        }
+        else
+        {
+            hoverTile = null;
+        }
+    }
+
+    private void ChangeTile()
+    {
+        if (hoverTile != null && Input.GetMouseButtonDown(0))
+        {
+            //hoverTile.GetComponent<SpriteRenderer>().sprite = selectedTile;
+        }
+    }
+
+    private void GenerateWorld()
+    {
+        for (int i = 0; i < worldSize; i++)
+        {
+            for (int j = 0; j < worldSize; j++)
+            {
+                GameObject cell = Instantiate(selectedTile);
+
+                cell.transform.position = new Vector2(i * .16f, j * .16f);
+            }
+        }
+    }
+}
